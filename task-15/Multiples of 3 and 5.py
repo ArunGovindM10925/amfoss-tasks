c5d87eac584a8084890f15bacae6530e76691794
Arun Govind M <67337602+ArunGovindM10925@users.noreply.github.com>
2020-11-30 20:24:47 +0530

    Update Multiples of 3 and 5.py

--- task-15/Multiples of 3 and 5.py
+++ task-15/Multiples of 3 and 5.py
@@ -1,11 +1,13 @@
-k=0
 t=int(input())
 for i in range(t):
     n=int(input())
+    sum=0
     if n<3 :
         print(0)
-    for j in range(3,n,3):
-        sum=sum+j
-        k=k+5
-        sum=sum+k
-    print(sum)
+    else:
+        for j in range(3,n):
+            if(j%3 == 0):
+                sum=sum+j
+            elif(j%5 == 0):
+                sum=sum+j
+        print(sum)
