39552d200cb196043bc462f95078cf8962287a0d
Arun Govind M <67337602+arungovindm2001@users.noreply.github.com>
2020-12-13 00:27:19 +0530

    Update task-8.py

--- task-8/task-8.py
+++ task-8/task-8.py
@@ -1,13 +1,13 @@
 
-from github import Github
+from github import Github   #imports the pygithub module
 
-import os
+import os   #imports os module
 
-username="amfoss"
+username="amfoss"   #sets the username variable to 'amfoss'
 
-user=Github().get_user(username)
+user=Github().get_user(username)    #gets the user details of amfoss
 
-for repo in user.get_repos():
+for repo in user.get_repos():   #get_repos() gets all the repositories from the user amfoss, stores it in a list and each element of it gets stored in repo
     print(repo.name)
-    os.system('perceval git --json-line https://github.com/amfoss/'+repo.name+'>> task-8.json')
-os.system('cat task-8.json')
+    os.system('perceval git --json-line https://github.com/amfoss/'+repo.name+'>> task-8.json') #receives the commits in each repo and exports it into the json file
+os.system('cat task-8.json')    #reads the json file
